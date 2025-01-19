Fixed the issue by removing the scaling from cyclic features

supervised_learning/rf_share_prediction.py
@@ -21,6 +21,11 @@ from utils.utils import fix_outliers
 
 df = pd.read_csv("datasets/sbi_historical.csv")
 
+# Sort the data by date
+df['Date'] = pd.to_datetime(df['Date'], dayfirst=True)
+df = df.sort_values(by='Date', ascending=True)
+
+
 # Fill missing data with mean and modes
 if df.isna().any().any(): 
     for column in df.select_dtypes(include=[np.number]).columns:
@@ -46,10 +51,14 @@ df["vol_10"] = df['Price'].rolling(window=10).std()
 df['mom_5'] = df['Price'] - df['Price'].shift(5)
 
 # Cycling features
-df['day_of_week'] = pd.to_datetime(df['Date'], dayfirst=True).dt.day_of_week
-df['week'] =  pd.to_datetime(df['Date'], dayfirst=True).dt.isocalendar().week
-df['month'] = pd.to_datetime(df['Date'], dayfirst=True).dt.month
-df['day_of_year'] = pd.to_datetime(df['Date'], dayfirst=True).dt.day_of_year
+# df['day_of_week'] = pd.to_datetime(df['Date'], dayfirst=True).dt.day_of_week
+# df['week'] =  pd.to_datetime(df['Date'], dayfirst=True).dt.isocalendar().week
+# df['month'] = pd.to_datetime(df['Date'], dayfirst=True).dt.month
+# df['day_of_year'] = pd.to_datetime(df['Date'], dayfirst=True).dt.day_of_year
+df['day_of_week'] = df['Date'].dt.day_of_week
+df['week'] = df['Date'].dt.isocalendar().week
+df['month'] = df['Date'].dt.month
+df['day_of_year'] = df['Date'].dt.day_of_year
 
 features = ['prev_price', 'prev_open', 'prev_high', 'prev_low', 'ma_5', 'ma_10',
             'vol_5', 'vol_10', 'mom_5', 'day_of_week', 'day_of_year', 'week', 'month']
@@ -59,10 +68,18 @@ X = df[features]
 y = df['Price']
 
 # Standardize the dataframe
-scaler = StandardScaler()
-X_s = scaler.fit_transform(X)
+features_to_scale = ['prev_price', 'prev_open', 'prev_high', 'prev_low', 'ma_5', 'ma_10',
+            'vol_5', 'vol_10', 'mom_5']
+temporal_features = ['day_of_week', 'day_of_year', 'week', 'month']
 
-X = pd.DataFrame(X_s)
+scaler = StandardScaler()
+X_s = scaler.fit_transform(X[features_to_scale])
+X_s = pd.DataFrame(X_s, columns=features_to_scale, index=X.index)
+
+X = pd.concat(
+    [X_s, X[temporal_features]],
+    axis=1
+)
 
 # Split the training and test data
 X_train, X_test, y_train, y_test = train_test_split(
@@ -87,10 +104,8 @@ print(f'R2 Score: {r2}')
 
 # Predict today price
 latest_data = df.iloc[-1]
-
 # Create prediction parameters for today
-today_test_data = pd.DataFrame(
-    {
+today_data = {
     'prev_price': [latest_data['Price']],
     'prev_open': [latest_data['Open']],
     'prev_high': [latest_data['High']],
@@ -104,19 +119,24 @@ today_test_data = pd.DataFrame(
     'day_of_year': [pd.Timestamp('today').day_of_year],
     'week': [pd.Timestamp('today').week],
     'month': [pd.Timestamp('today').month]
-    }
+}
+today_test_data = pd.DataFrame(
+    today_data
 )
 
-today_data_scaled = scaler.transform(today_test_data)
-
-today_prediction = model.predict(today_data_scaled)
+today_data_s = scaler.transform(today_test_data[features_to_scale])
+today_data_s = pd.DataFrame(today_data_s, columns=features_to_scale, index=today_test_data.index)
+today_test_data = pd.concat(
+    [today_data_s, today_test_data[temporal_features]],
+    axis=1
+)
+today_prediction = model.predict(today_test_data)
 
 print(f"Today's Prediction: {today_prediction}")
 
 '''
-Output:-
-Mean Square Error: 16.306501368000006
-Mean Absolute Error: 2.3144120000000084
-R2 Score: 0.9994998607520263
-Today's Prediction: [310.7515] # This is highly wrong :)
+Mean Square Error: 51.48483545699991
+Mean Absolute Error: 4.71052066666666
+R2 Score: 0.9984810871053948
+Today's Prediction: [765.2525] current value is 764
 '''